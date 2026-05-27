aa23e05142636975708ffbc82b91f03103eb38c4
Betweenthebells <dallascmcdougall@gmail.com>
2026-05-27 14:39:26 +1000

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TeacherDayPlanner from './TeacherDayPlanner';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TeacherDayPlanner />
+    </div>
+  );
+}
+
+export default App;
